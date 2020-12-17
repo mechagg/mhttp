41a98f0a1cd44bbfdea9b8e8b02e27a3a78dd863
jackk <j4ckk3@gmail.com>
2020-12-16 18:33:27 -0600

    added basic mhttp lua function

--- module/src/main.cpp
+++ module/src/main.cpp
@@ -4,8 +4,27 @@
 
 using namespace GarrysMod::Lua;
 
+LUA_FUNCTION(mhttp_impl)
+{
+	LUA->CheckType(1, Type::String);
+
+	LUA->PushSpecial(SPECIAL_GLOB);
+		LUA->GetField(-1, "print");
+		LUA->Push(1);
+		LUA->Call(1, 0);
+	LUA->Pop();
+
+	return 0;
+}
+
 GMOD_MODULE_OPEN()
 {
+	// Push the "mhttp" function onto the global stack.
+	LUA->PushSpecial(SPECIAL_GLOB);
+		LUA->PushCFunction(mhttp_impl);
+		LUA->SetField(-2, "mhttp");
+	LUA->Pop();
+
 	return 0;
 }
 
